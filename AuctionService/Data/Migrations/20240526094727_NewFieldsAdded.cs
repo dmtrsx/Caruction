9b932e756086aca2196e217f65d109f26e7dce7c
Dmytro Shyika <drghost9899@gmail.com>
2024-05-28 20:03:21 +0300

    Initial Commit

---
+++ AuctionService/Data/Migrations/20240526094727_NewFieldsAdded.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewFieldsAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Items",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Mileage",
+                table: "Items",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "AuctionEnd",
+                table: "Auctions",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "Mileage",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "AuctionEnd",
+                table: "Auctions");
+        }
+    }
+}
